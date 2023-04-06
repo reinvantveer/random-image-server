a5f60deabaed3dd1c06b00cb8948e014abb04fbc
reinvantveer <reinvantveer@gmail.com>
2023-04-06 21:08:47 +0200

    break out main function

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+import logging
 import random
 from argparse import ArgumentParser
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, render_template
 
-from common.config import load_config
+from common.config import load_config, Config
 
 app = Flask(__name__)
+
+
+def main(config: Config):
+    images_path_ = config['images']['path']
+
+    logging.info(f"Fetching image file list for {images_path_}")
+    file_list = []
+
+    for ext in config['images']['filetypes']:
+        file_list.extend(Path(images_path_).rglob(ext))
+
+    logging.info('Building list')
+    file_list = list(file_list)
+
+    @app.route("/")
+    def hello() -> str:
+        random_img = random.randint(0, len(file_list))
+        img_ = str(file_list[random_img])
+        return render_template("index.html", user_image=full_filename)
+
+    app.run()
 
 
 if __name__ == '__main__':
@@ -16,13 +38,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     config = load_config(args.config)
 
-    file_list = []
-    for ext in config['images']['filetypes']:
-        file_list.extend(Path(config['images']['path']).glob(ext))
-
-    @app.route("/")
-    def hello() -> str:
-        random_img = random.randint(0, len(file_list))
-        return f"<img src={random_img}>Hello</img>"
-
-    app.run()
+    main(config)
