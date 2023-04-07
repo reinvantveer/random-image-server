ae8d146fa0ee63fb55091b261886374176aac8b9
reinvantveer <reinvantveer@gmail.com>
2023-04-07 20:07:34 +0200

    fix image loading & serving

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import random
 from argparse import ArgumentParser
 from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 from common.config import load_config, Config
-
-app = Flask(__name__)
 
 
 def main(config: Config):
     images_path_ = config['images']['path']
+    app = Flask(__name__,
+                static_url_path='/static',
+                static_folder=images_path_)
 
     logging.info(f"Fetching image file list for {images_path_}")
     file_list = []
@@ -25,8 +26,9 @@ def main(config: Config):
     @app.route("/")
     def hello() -> str:
         random_img = random.randint(0, len(file_list))
-        img_ = str(file_list[random_img])
-        return render_template("index.html", user_image=img_)
+        img_path = file_list[random_img]
+        img_relative_to_static_folder = str(img_path).replace(images_path_, '/static')
+        return render_template("index.html", user_image=img_relative_to_static_folder, local_path=img_path)
 
     app.run()
 
